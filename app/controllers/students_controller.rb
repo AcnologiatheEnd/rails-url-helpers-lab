8c11c46a196a25c61b2fb977e5a42287f287b0ac
Winston  Chung <18.chung.winston@ctkprep.org>
2020-01-17 06:37:41 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,6 +7,19 @@ class StudentsController < ApplicationController
 
   def show
   end
+  
+  def activate
+    @student = Student.find(params[:id])
+    if @student.active == false
+      @student.active = true
+      @student.save
+      redirect_to student_path(@student)
+    else
+      @student.active = false
+      @student.save
+      redirect_to student_path(@student)
+    end
+  end
 
   private
 
